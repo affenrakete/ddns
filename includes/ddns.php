0142245a50d878d8590cc65d86af255e26ef54e2
Affenrakete <postmaster@affenrakete.de>
2017-02-27 22:31:45 +0100

    include logger

--- includes/ddns.php
+++ includes/ddns.php
@@ -2,6 +2,10 @@
 
 namespace Affenrakete;
 
+use INWX\Domrobot;
+use Katzgrau\KLogger\Logger;
+use Psr\Log\LogLevel;
+
 /**
  * INWX DDNS Manager
  *
@@ -12,6 +16,7 @@ namespace Affenrakete;
  */
 class DDNS {
 
+    protected $logFilePath = "./logs/";
     protected $iniFilePath = "./conf/";
     protected $iniFileInwx = "inwx.ini";
     protected $iniFileDomain = "";
@@ -20,47 +25,56 @@ class DDNS {
     protected $IP4 = [];        // {oldip, newip, id}
     protected $IP6 = [];        // {oldip, newip, id}
     protected $domrobot;
+    protected $logger;
 
     public function __construct($apidomain = null, $apikey = null) {
-        $this->setIniFileDomain($apidomain);
-        $this->readIni();
-        $this->checkAccess($apikey);
+        self::setIniFile($apidomain);
+        self::startLogger();
+        self::readIni();
+        self::checkAccess($apikey);
     }
 
-    protected function setIniFileDomain($apidomain = null) {
+    protected function precheck($str = null) {
+        return htmlspecialchars(trim($str), ENT_QUOTES, 'UTF-8');
+    }
+
+    protected function setIniFile($apidomain = null) {
         $domainReturn = [];
 
         preg_match("/(?!.{253})((?!-)[A-Za-z0-9-]{1,63}(?<!-)\.){1,126}+[A-Za-z]{2,6}/", $apidomain, $domainReturn);
         $domainReturn[0] = str_replace('.', '-', $domainReturn[0]);
 
-        $this->iniFileDomain = $domainReturn[0] . '.ini';
+        $this->iniFileDomain = $domainReturn[0];
 
         return;
     }
 
-    protected function precheck($str) {
-        $str = htmlspecialchars(trim($str), ENT_QUOTES, 'UTF-8');
+    protected function startLogger() {
+        $logLevel = LogLevel::INFO;
+        if (DEBUG) {
+            $logLevel = LogLevel::DEBUG;
+        }
 
-        if (empty($str))
-            exit('something went wrong 7');
-
-        return $str;
+        $this->logger = new Logger($this->logFilePath . $this->iniFileDomain, $logLevel);
     }
 
     protected function readIni() {
         // check if ini files exists
-        if (!file_exists($this->iniFilePath . $this->iniFileDomain))
-            exit('something went wrong 8');
-        if (!file_exists($this->iniFilePath . $this->iniFileInwx))
-            exit('something went wrong 1');
+        if (!file_exists($this->iniFilePath . $this->iniFileDomain . ".ini")) {
+            $this->logger->error('File does not exists | iniFileDomain :' . $this->iniFileDomain . '.ini');
+            return false;
+        }
+        if (!file_exists($this->iniFilePath . $this->iniFileInwx)) {
+            $this->logger->error('File does not exists | iniFileDomain :' . $this->iniFileInwx);
+            return false;
+        }
 
         // read domain.ini
-        $ini = parse_ini_file($this->iniFilePath . $this->iniFileDomain, TRUE);
+        $ini = parse_ini_file($this->iniFilePath . $this->iniFileDomain . ".ini", TRUE);
 
         $this->domain['inwx']['domain'] = self::precheck($ini['inwx']['domain']);
         $this->domain['inwx']['subdomain'] = self::precheck($ini['inwx']['subdomain']);
         $this->domain['ddns']['apikey'] = self::precheck($ini['ddns']['apikey']);
-        $this->domain['log']['filepath'] = self::precheck($ini['log']['filepath']);
 
         //read inwx.ini
         $ini = parse_ini_file($this->iniFilePath . $this->iniFileInwx, TRUE);
@@ -73,47 +87,53 @@ class DDNS {
     }
 
     protected function checkAccess($apikey = null) {
-        if ($apikey == null || $this->domain['ddns']['apikey'] !== $apikey)
-            exit('something went wrong 3');
+        if ($apikey == null || $this->domain['ddns']['apikey'] !== $apikey) {
+            $this->logger->error('unauthorisized access from IP: ' . $_SERVER['REMOTE_ADDR']);
+            return false;
+        }
 
-        return;
+        return true;
     }
 
     protected function inwxLogin() {
         // INWX Setup class
-        $this->domrobot = new \INWX\domrobot($this->inwx['apiurl']);
+        $this->domrobot = new Domrobot($this->inwx['apiurl']);
         $this->domrobot->setDebug(false);
         $this->domrobot->setLanguage('en');
 
         // INWX Login
         $result = $this->domrobot->login($this->inwx['username'], $this->inwx['password']);
 
-        if (DEBUG)
-            print_r($result);
+        $this->logger->debug($result);
 
         // check result
         if ($result['code'] != 1000) {
-            if (OUTPUT)
-                exit('badauth');
 
-            exit('something went wrong 4');
+            if (OUTPUT) {
+                echo('badauth');
+            }
+
+            $this->logger->error('inwx login not successfull');
+            return false;
         }
+        $this->logger->debug('inwx login successfull');
 
-        return;
+        return true;
     }
 
-    protected function inwxLogout() {
+    public function inwxLogout() {
         $result = $this->domrobot->logout();
 
-        if (DEBUG)
-            print_r($result);
+        $this->logger->debug($result);
 
         // check result
         if ($result['code'] != 1500) {
-            exit('something went wrong 5');
+            $this->logger->error('inwx logout NOT successfull');
+            return false;
         }
+        $this->logger->debug('inwx logout successfull');
 
-        return;
+        return true;
     }
 
     protected function inwxGetNameserverInfo() {
@@ -126,12 +146,13 @@ class DDNS {
 
         $result = $this->domrobot->call($object, $methode, $params);
 
-        if (DEBUG)
-            print_r($result);
+
+        $this->logger->debug($result);
 
         // check result
         if ($result['code'] != 1000) {
-            exit('something went wrong 6');
+            $this->logger->error('get nameserver info NOT successfull');
+            return false;
         }
 
         foreach ($result["resData"]["record"] as $value) {
@@ -145,7 +166,9 @@ class DDNS {
             }
         }
 
-        return;
+        $this->logger->debug('get nameserver info successfull');
+
+        return true;
     }
 
     protected function inwxSetNameserverInfo($type = null) {
@@ -157,46 +180,65 @@ class DDNS {
         if ($type == 'ipv4') {
             $params['id'] = $this->IP4['id'];
             $params['content'] = $this->IP4['newip'];
+            $oldip = $this->IP4['oldip'];
         } elseif ($type == 'ipv6') {
             $params['id'] = $this->IP6['id'];
             $params['content'] = $this->IP6['newip'];
-        } else
-            exit('something went wrong 9');
+            $oldip = $this->IP6['oldip'];
+        } else {
+            $this->logger->warning('set nameserver info type: ' . $type);
+            return false;
+        }
 
         $result = $this->domrobot->call($object, $methode, $params);
 
-        if (DEBUG)
-            print_r($result);
+        $this->logger->debug($result);
 
         // check result
         if ($result['code'] != 1000) {
-            exit('something went wrong 10');
+            $this->logger->error('set nameserver info NOT successfull');
+            return false;
         }
 
-        if (OUTPUT)
-            echo('good');
+        $this->logger->debug('set nameserver info successfull');
+        $this->logger->info('IP Update successfull | old ip: ' . $oldip . ' | new ip: ' . $params['content']);
 
-        return;
+        return true;
     }
 
     public function updateIP($ipv4 = null, $ipv6 = null) {
-        $this->inwxLogin();
 
-        $this->inwxGetNameserverInfo();
+        if (!self::inwxLogin()) {
+            return false;
+        }
+
+        if (!self::inwxGetNameserverInfo()) {
+            return false;
+        }
 
         if (filter_var($ipv4, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4)) {
             $this->IP4['newip'] = $ipv4;
-            $this->inwxSetNameserverInfo('ipv4');
+
+            if (!self::inwxSetNameserverInfo('ipv4')) {
+                return false;
+            }
         }
 
         if (filter_var($ipv6, FILTER_VALIDATE_IP, FILTER_FLAG_IPV6)) {
             $this->IP6['newip'] = $ipv6;
-            $this->inwxSetNameserverInfo('ipv6');
+
+            if (!self::inwxSetNameserverInfo('ipv6')) {
+                return false;
+            }
         }
 
-        $this->inwxLogout();
+        if (OUTPUT) {
+            echo('good');
+        }
 
-        return;
+        $this->logger->debug('updateIp successfull');
+
+        return true;
     }
 
 }
--- update.php
+++ update.php
@@ -10,7 +10,11 @@ namespace Affenrakete;
  * @link https://affenrakete.de
  *
  */
+
 header('Content-type: text/plain; charset=utf-8'); // Set UFT-8 Header
+
+require_once('./includes/ddns.php');
+require_once('./vendor/autoload.php');
 
 define("DEBUG", true);
 define("OUTPUT", true);
@@ -20,16 +24,11 @@ if (DEBUG) {
     ini_set("display_errors", 1);
 }
 
-require_once('./includes/ddns.php');
-require_once('./libraries/domrobot.php');
-
 $input['domain'] = filter_has_var(INPUT_GET, 'domain') ? filter_input(INPUT_GET, 'domain', FILTER_VALIDATE_REGEXP, ['options' => ['regexp' => '/(?!.{253})((?!-)[A-Za-z0-9-]{1,63}(?<!-)\.){1,126}+[A-Za-z]{2,6}/']]) : null;
 $input['password'] = filter_has_var(INPUT_GET, 'password') ? filter_input(INPUT_GET, 'password') : null;
 $input['ipv4'] = filter_has_var(INPUT_GET, 'ipv4') ? filter_input(INPUT_GET, 'ipv4', FILTER_VALIDATE_IP, FILTER_FLAG_IPV4) : null;
 $input['ipv6'] = filter_has_var(INPUT_GET, 'ipv6') ? filter_input(INPUT_GET, 'ipv6', FILTER_VALIDATE_IP, FILTER_FLAG_IPV6) : null;
 
-if (DEBUG)
-    print_r($input);
-
 $ddns = new DDNS($input['domain'], $input['password']);
 $ddns->updateIP($input['ipv4'], $input['ipv6']);
+$ddns->inwxLogout();
